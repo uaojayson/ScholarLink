9dbefb5e935460ab3d471bd95902748c9636b32e
uaojayson <uaojaysongonzalo@gmail.com>
2023-04-30 12:48:11 +0800

    new file:   css/navbar.css 	modified:   index.html 	new file:   scholarships/scholarship-assets/logo.png 	new file:   scholarships/scholarship.css 	new file:   scholarships/scholarship.html 	new file:   scholarships/scholarship.js

---
+++ scholarships/scholarship.js
@@ -0,0 +1,10 @@
+function setActiveLink(link) {
+    // Remove the "active" class from all links
+    var links = document.querySelectorAll('.nav-link');
+    for (var i = 0; i < links.length; i++) {
+      links[i].classList.remove('active');
+    }
+  
+    // Add the "active" class to the clicked link
+    link.classList.add('active');
+  }
