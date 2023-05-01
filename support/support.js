f910f5e3b8b92cabe22b839f597c8e7cd5c31219
uaojayson <uaojaysongonzalo@gmail.com>
2023-05-01 13:48:55 +0800

    modified:   index.html 	modified:   scholarships/government.html 	modified:   scholarships/ngo.html 	modified:   scholarships/private.html 	modified:   scholarships/scholarship.html 	modified:   support/support.css 	modified:   support/support.html 	modified:   support/support.js 	new file:   support/terms-conditions.html

--- support/support.js
+++ support/support.js
@@ -36,3 +36,30 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+
+var tab_lists = document.querySelectorAll(".tabs_list ul li");
+var tab_items = document.querySelectorAll(".tab_item"); 
+
+tab_lists.forEach(function(list){
+  list.addEventListener("click", function(){
+    var tab_data = list.getAttribute("data-tc");
+    
+    tab_lists.forEach(function(list){
+      list.classList.remove("active");
+    });
+    list.classList.add("active");
+    
+    tab_items.forEach(function(item){
+      var tab_class = item.getAttribute("class").split(" ");
+      if(tab_class.includes(tab_data)){
+        item.style.display = "block";
+      }
+      else{
+        item.style.display = "none";
+      }
+      
+    })
+    
+  })
+})
