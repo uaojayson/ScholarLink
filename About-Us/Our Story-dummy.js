7064101c895a387d2a6e3eaa38c9d9e75369c07d
kodegoCJ <gambalacharles96gmail.com>
2023-05-03 18:32:09 +0800

    Fixed the css inconsistency in our story page

--- About-Us/Our Story-dummy.js
+++ About-Us/Our Story-dummy.js
@@ -1,3 +1,4 @@
+// navbar 
 function setActiveLink(link) {
     // Remove the "active" class from all links
     var links = document.querySelectorAll('.nav-link');
@@ -9,47 +10,47 @@ function setActiveLink(link) {
     link.classList.add('active');
   }
 
+// Carousel
+	const slider = document.querySelector(".items");
+	const slides = document.querySelectorAll(".item");
+	const button = document.querySelectorAll(".button");
 
-const slider = document.querySelector(".items");
-		const slides = document.querySelectorAll(".partner-item");
-		const button = document.querySelectorAll(".button");
+	let current = 0;
+	let prev = 4;
+	let next = 1;
 
-		let current = 0;
-		let prev = 4;
-		let next = 1;
+	for (let i = 0; i < button.length; i++) {
+		button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+	}
 
-		for (let i = 0; i < button.length; i++) {
-			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+	const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+	const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+	const gotoNum = number => {
+		current = number;
+		prev = current - 1;
+		next = current + 1;
+
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].classList.remove("active");
+			slides[i].classList.remove("prev");
+			slides[i].classList.remove("next");
 		}
 
-		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
-
-		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
-
-		const gotoNum = number => {
-			current = number;
-			prev = current - 1;
-			next = current + 1;
-
-			for (let i = 0; i < slides.length; i++) {
-				slides[i].classList.remove("active");
-				slides[i].classList.remove("prev");
-				slides[i].classList.remove("next");
-			}
-
-			if (next == 5) {
-				next = 0;
-			}
-
-			if (prev == -1) {
-				prev = 4;
-			}
-
-			slides[current].classList.add("active");
-			slides[prev].classList.add("prev");
-			slides[next].classList.add("next");
+		if (next == 5) {
+			next = 0;
 		}
 
+		if (prev == -1) {
+			prev = 4;
+		}
+
+		slides[current].classList.add("active");
+		slides[prev].classList.add("prev");
+		slides[next].classList.add("next");
+	}
+// Login page 
 		// Get the login and signup buttons
 const loginBtn = document.querySelector('.btn-login');
 
